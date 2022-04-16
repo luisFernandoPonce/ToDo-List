6fb30fac24f1d3015cf7347e16306821103a272c
jackbasso <jackbasso.r@gmail.com>
2022-04-16 18:10:22 -0500

    remove option ready by jack

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import TodoForm from './TodoForm'
+import Todo from './Todo';
 
 function TodoList() {
   const [todos, setTodos] = useState([])
@@ -8,17 +9,37 @@ function TodoList() {
     if (!todo.text || /^\s*$/.test(todo.text)) {
       return;
     }
-  
+    
   const nuevoTodo = [todo, ...todos]
 
   setTodos(nuevoTodo)
-  console.log(...todos)
+  console.log(todo, ...todos) // visualización en tiempo real
   };
+
+  const removeTodo = id => {
+    const removeArr = [...todos].filter(todo => todo.id !== id);
+    setTodos(removeArr)
+  }
+
+  const completeTodo = id => {
+    let updatedTodos = todos.map(todo => {
+      if (todo.id === id){
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos)
+  }
 
   return (
     <div>
       <h1>Que tareas tenemos para hoy?</h1>
       <TodoForm onSubmit={agregarTodo} />
+      <Todo 
+        todos = {todos}
+        completeTodo = {completeTodo}
+        removeTodo = {removeTodo}
+      />
     </div>
   )
 }
